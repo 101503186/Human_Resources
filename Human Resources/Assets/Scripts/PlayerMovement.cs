e5902d7213b2421e12c8416786268e8663cf42ac
SenkoSlab <148092513+101503186@users.noreply.github.com>
2026-01-27 19:12:01 -0500

    basic player movement complete - sprinting, walking, jumping, movement not locked to camera

--- Human Resources/Assets/Scripts/PlayerMovement.cs
+++ Human Resources/Assets/Scripts/PlayerMovement.cs
@@ -3,44 +3,75 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float playerSpeed = 5.0f;
+    private float walkSpeed = 6.0f;
+    private float sprintSpeed = 10.0f;
     private float jumpHeight = 1.5f;
-    private float gravityValue = -9.81f;
+    private float gravityValue = -14.7f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
 
     public CharacterController controller;
+    public Transform cam;
     private Vector3 playerVelocity;
+    private bool groundedPlayer;
 
     [Header("Input Actions")]
     public InputActionReference moveAction;
     public InputActionReference jumpAction;
+    public InputActionReference sprintAction;
 
     private void OnEnable()
     {
         moveAction.action.Enable();
         jumpAction.action.Enable();
+        sprintAction.action.Enable();
     }
 
     private void OnDisable()
     {
         moveAction.action.Disable();
         jumpAction.action.Disable();
+        sprintAction.action.Disable();
     }
 
     void Update()
     {
+        groundedPlayer = controller.isGrounded;
+
+        if (groundedPlayer)
+        {
+            if (playerVelocity.y < -2f)
+            {
+                playerVelocity.y = -2f;
+            }
+        }
+
         // Read input
         Vector2 input = moveAction.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
-        move = Vector3.ClampMagnitude(move, 1f);
+        Vector3 direction = new Vector3(input.x, 0, input.y).normalized;
 
-        if (move != Vector3.zero)
-            transform.forward = move;
+        bool isSprinting = sprintAction.action.IsPressed();
+        float currentSpeed = isSprinting ? sprintSpeed : walkSpeed;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
+        }
+
+        if (groundedPlayer && jumpAction.action.WasPressedThisFrame())
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+        }
 
         // Apply gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
 
-        // Move
-        Vector3 finalMove = move * playerSpeed + Vector3.up * playerVelocity.y;
-        controller.Move(finalMove * Time.deltaTime);
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
