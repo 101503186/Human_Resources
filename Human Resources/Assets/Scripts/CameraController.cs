66da1596cad27ed73dacf3a8896736b1758ee9c2
SenkoSlab <148092513+101503186@users.noreply.github.com>
2026-03-17 00:10:05 -0400

    camera controller script created (changes between regular camera and aiming camera)

---
+++ Human Resources/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera aimVirtualCamera;
+    [SerializeField] private CinemachineCamera regularVirtualCamera;
+    [SerializeField] private RawImage crosshairImage;
+    public PlayerMovement player;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (player.isAiming)
+        {
+            aimVirtualCamera.Priority = 20;
+            regularVirtualCamera.Priority = 10;
+            crosshairImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            aimVirtualCamera.Priority = 10;
+            regularVirtualCamera.Priority = 20;
+            crosshairImage.gameObject.SetActive(false);
+        }
+    }
+}
